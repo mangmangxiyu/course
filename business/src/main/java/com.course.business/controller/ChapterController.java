7dd62e6f97509a06d53f10073eef944efbf7a1f5
tom_glb <goodMoring_glb@qq.com>
2021-03-07 18:33:23 +0800

    1.增加大章列表查询接口

---
+++ business/src/main/java/com.course.business/controller/ChapterController.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C), 2015-2021, XXX有限公司
+ * FileName: ChapterController
+ * Author:   111
+ * Date:     2021/3/3 13:17
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.course.business.controller;/**
+ * Created by 111 on 2021/3/3.
+ */
+
+import com.course.server.domain.Chapter;
+import com.course.server.service.ChapterService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author 111
+ * @create 2021/3/3
+ * @since 1.0.0
+ */
+
+@RestController
+public class ChapterController {
+
+    @Resource
+    private ChapterService chapterService;
+
+    @RequestMapping("/chapter")
+    public List<Chapter> chapter() {
+        return chapterService.list();
+    }
+}
---
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C), 2015-2021, XXX有限公司
+ * FileName: ChapterService
+ * Author:   111
+ * Date:     2021/3/7 18:15
+ * Description: 大章的service业务逻辑代码
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.course.server.service;
+
+import com.course.server.domain.Chapter;
+import com.course.server.domain.ChapterExample;
+import com.course.server.mapper.ChapterMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈大章的service业务逻辑代码〉
+ *
+ * @author 111
+ * @create 2021/3/7
+ * @since 1.0.0
+ */
+@Service
+public class ChapterService {
+
+    @Resource
+    private ChapterMapper chapterMapper;
+
+    public List<Chapter> list() {
+        ChapterExample chapterExample = new ChapterExample();
+        chapterExample.createCriteria().andIdEqualTo("1");
+        chapterExample.setOrderByClause("id desc");
+        return chapterMapper.selectByExample(chapterExample);
+    }
+
+}
