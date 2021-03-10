0cd0d7f316a19a84159d4afe01f2469fb9f9797f
tom_glb <goodMoring_glb@qq.com>
2021-03-10 22:11:06 +0800

    1. 增加大章新增功能，前端代码开发 2. eslint关闭no-unused-vars校验

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <p>
-      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
-      <i class="ace-icon fa fa-refresh red2"></i>
+    <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+      <i class="ace-icon fa fa-edit"></i>
+      新增
+    </button>
+    &nbsp;
+    <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
+      <i class="ace-icon fa fa-refresh"></i>
       刷新
     </button>
     </p>
@@ -82,6 +87,37 @@
       </tr>
       </tbody>
     </table>
+
+    <div class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">表单</h4>
+          </div>
+          <div class="modal-body">
+            <form class="form-horizontal">
+              <div class="form-group">
+                <label class="col-sm-2 control-label">名称</label>
+                <div class="col-sm-10">
+                  <input class="form-control" placeholder="名称">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">课程ID</label>
+                <div class="col-sm-10">
+                  <input class="form-control" placeholder="课程ID">
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+            <button type="button" class="btn btn-primary">保存</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
   </div>
 </template>
 
@@ -104,6 +140,11 @@
       // 前后端数据交互
     },
     methods: {
+      add() {
+        let _this = this;
+        // 第一个modal是css选择器，模态框代码里有class="modal",是modal()方法里的是内置方法，弹出关闭模态框hide，show
+        $(".modal").modal({backdrop:"static"}, "show");
+      },
       list(page) {
         let _this = this;
         _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
