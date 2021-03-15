2f03bca5bf842b969f8e730da632631a851289eb
mangmangxiyu <945353778@qq.com>
2021-03-15 23:01:27 +0800

    1. 课程管理页面美化style：scope防止组件污染 2. image\一些图片文件

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -15,84 +15,127 @@
     <!--组件的位置并进行渲染:第一个list组件内部定义的回调方法，第二个是course组件的方法list-->
     <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
 
-    <table id="simple-table" class="table  table-bordered table-hover">
-      <thead>
-      <tr>
-        <th>id</th>
-        <th>名称</th>
-        <th>概述</th>
-        <th>时长</th>
-        <th>价格（元）</th>
-        <th>封面</th>
-        <th>级别</th>
-        <th>收费</th>
-        <th>状态</th>
-        <th>报名数</th>
-        <th>顺序</th>
-        <th>操作</th>
-      </tr>
-      </thead>
+    <div class="row">
+      <div v-for="course in courses" class="col-md-4">
+        <div class="thumbnail search-thumbnail">
+          <img v-show="!course.image" class="media-object" src="/static/image/demo-course.jpg" />
+          <img v-show="course.image" class="media-object" v-bind:src="course.image" />
+          <div class="caption">
+            <div class="clearfix">
+              <span class="pull-right label label-primary info-label">
+                {{COURSE_LEVEL | optionKV(course.level)}}
+              </span>
+              <span class="pull-right label label-primary info-label">
+                {{COURSE_CHARGE | optionKV(course.charge)}}
+              </span>
+              <span class="pull-right label label-primary info-label">
+                {{COURSE_STATUS | optionKV(course.status)}}
+              </span>
+            </div>
 
-      <tbody>
-      <tr v-for="course in courses">
-        <td>{{course.id}}</td>
-        <td>{{course.name}}</td>
-        <td>{{course.summary}}</td>
-        <td>{{course.time}}</td>
-        <td>{{course.price}}</td>
-        <td>{{course.image}}</td>
-        <td>{{COURSE_LEVEL | optionKV(course.level)}}</td>
-        <td>{{COURSE_CHARGE | optionKV(course.charge)}}</td>
-        <td>{{COURSE_STATUS | optionKV(course.status)}}</td>
-        <td>{{course.enroll}}</td>
-        <td>{{course.sort}}</td>
-      <td>
-        <div class="hidden-sm hidden-xs btn-group">
-          <button v-on:click="edit(course)" class="btn btn-xs btn-info">
-            <i class="ace-icon fa fa-pencil bigger-120"></i>
-          </button>
-          <button v-on:click="del(course.id)" class="btn btn-xs btn-danger">
-            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-          </button>
-        </div>
-
-        <div class="hidden-md hidden-lg">
-          <div class="inline pos-rel">
-            <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-              <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-            </button>
-
-            <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-              <li>
-                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-                  <span class="blue">
-                    <i class="ace-icon fa fa-search-plus bigger-120"></i>
-                  </span>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-                  <span class="green">
-                    <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-                  </span>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-                  <span class="red">
-                    <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                  </span>
-                </a>
-              </li>
-            </ul>
+            <h3 class="search-title">
+              <a href="#" class="blue">{{course.name}}</a>
+            </h3>
+            <p>
+              <span class="blue bolder bigger-150">{{course.price}}&nbsp;<i class="fa fa-rmb"></i></span>&nbsp;
+            </p>
+            <p>{{course.summary}}</p>
+            <p>
+              <span class="badge badge-info">{{course.id}}</span>
+              <span class="badge badge-info">排序：{{course.sort}}</span>
+              <span class="badge badge-info">时长：{{course.time}}</span>
+            </p>
+            <p>
+              <button v-on:click="edit(course)" class="btn btn-white btn-xs btn-info btn-round">
+                编辑
+              </button>&nbsp;
+              <button v-on:click="del(course.id)" class="btn btn-white btn-xs btn-warning btn-round">
+                删除
+              </button>
+            </p>
           </div>
         </div>
-      </td>
-      </tr>
-      </tbody>
-    </table>
+      </div>
+    </div>
+
+    <!--<table id="simple-table" class="table  table-bordered table-hover">-->
+      <!--<thead>-->
+      <!--<tr>-->
+        <!--<th>id</th>-->
+        <!--<th>名称</th>-->
+        <!--<th>概述</th>-->
+        <!--<th>时长</th>-->
+        <!--<th>价格（元）</th>-->
+        <!--<th>封面</th>-->
+        <!--<th>级别</th>-->
+        <!--<th>收费</th>-->
+        <!--<th>状态</th>-->
+        <!--<th>报名数</th>-->
+        <!--<th>顺序</th>-->
+        <!--<th>操作</th>-->
+      <!--</tr>-->
+      <!--</thead>-->
+
+      <!--<tbody>-->
+      <!--<tr v-for="course in courses">-->
+        <!--<td>{{course.id}}</td>-->
+        <!--<td>{{course.name}}</td>-->
+        <!--<td>{{course.summary}}</td>-->
+        <!--<td>{{course.time}}</td>-->
+        <!--<td>{{course.price}}</td>-->
+        <!--<td>{{course.image}}</td>-->
+        <!--<td>{{COURSE_LEVEL | optionKV(course.level)}}</td>-->
+        <!--<td>{{COURSE_CHARGE | optionKV(course.charge)}}</td>-->
+        <!--<td>{{COURSE_STATUS | optionKV(course.status)}}</td>-->
+        <!--<td>{{course.enroll}}</td>-->
+        <!--<td>{{course.sort}}</td>-->
+      <!--<td>-->
+        <!--<div class="hidden-sm hidden-xs btn-group">-->
+          <!--<button v-on:click="edit(course)" class="btn btn-xs btn-info">-->
+            <!--<i class="ace-icon fa fa-pencil bigger-120"></i>-->
+          <!--</button>-->
+          <!--<button v-on:click="del(course.id)" class="btn btn-xs btn-danger">-->
+            <!--<i class="ace-icon fa fa-trash-o bigger-120"></i>-->
+          <!--</button>-->
+        <!--</div>-->
+
+        <!--<div class="hidden-md hidden-lg">-->
+          <!--<div class="inline pos-rel">-->
+            <!--<button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">-->
+              <!--<i class="ace-icon fa fa-cog icon-only bigger-110"></i>-->
+            <!--</button>-->
+
+            <!--<ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">-->
+              <!--<li>-->
+                <!--<a href="#" class="tooltip-info" data-rel="tooltip" title="View">-->
+                  <!--<span class="blue">-->
+                    <!--<i class="ace-icon fa fa-search-plus bigger-120"></i>-->
+                  <!--</span>-->
+                <!--</a>-->
+              <!--</li>-->
+
+              <!--<li>-->
+                <!--<a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">-->
+                  <!--<span class="green">-->
+                    <!--<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>-->
+                  <!--</span>-->
+                <!--</a>-->
+              <!--</li>-->
+
+              <!--<li>-->
+                <!--<a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">-->
+                  <!--<span class="red">-->
+                    <!--<i class="ace-icon fa fa-trash-o bigger-120"></i>-->
+                  <!--</span>-->
+                <!--</a>-->
+              <!--</li>-->
+            <!--</ul>-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</td>-->
+      <!--</tr>-->
+      <!--</tbody>-->
+    <!--</table>-->
 
     <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
@@ -290,3 +333,9 @@
     }
   }
 </script>
+
+<style scoped>
+  .caption h3 {
+    font-size: 20px;
+  }
+</style>
