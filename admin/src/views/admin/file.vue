e3054a37afca998561f68914c0af28c1a9d08697
mangmangxiyu <945353778@qq.com>
2021-03-19 16:45:24 +0800

    1. 上传文件功能开发：去掉文件管理的新增、修改、删除功能

--- admin/src/views/admin/file.vue
+++ admin/src/views/admin/file.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <p>
-      <button v-on:click="add()" class="btn btn-white btn-default btn-round">
-        <i class="ace-icon fa fa-edit"></i>
-        新增
-      </button>
-      &nbsp;
       <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
         <i class="ace-icon fa fa-refresh"></i>
         刷新
@@ -24,7 +19,6 @@
         <th>后缀</th>
         <th>大小</th>
         <th>用途</th>
-        <th>操作</th>
       </tr>
       </thead>
 
@@ -36,104 +30,9 @@
         <td>{{file.suffix}}</td>
         <td>{{file.size}}</td>
         <td>{{FILE_USE | optionKV(file.use)}}</td>
-      <td>
-        <div class="hidden-sm hidden-xs btn-group">
-          <button v-on:click="edit(file)" class="btn btn-xs btn-info">
-            <i class="ace-icon fa fa-pencil bigger-120"></i>
-          </button>
-          <button v-on:click="del(file.id)" class="btn btn-xs btn-danger">
-            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-          </button>
-        </div>
-
-        <div class="hidden-md hidden-lg">
-          <div class="inline pos-rel">
-            <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-              <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-            </button>
-
-            <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-              <li>
-                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-                  <span class="blue">
-                    <i class="ace-icon fa fa-search-plus bigger-120"></i>
-                  </span>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-                  <span class="green">
-                    <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-                  </span>
-                </a>
-              </li>
-
-              <li>
-                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-                  <span class="red">
-                    <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </td>
       </tr>
       </tbody>
     </table>
-
-    <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">表单</h4>
-          </div>
-          <div class="modal-body">
-            <form class="form-horizontal">
-              <div class="form-group">
-                <label class="col-sm-2 control-label">相对路径</label>
-                <div class="col-sm-10">
-                  <input v-model="file.path" class="form-control">
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-sm-2 control-label">文件名</label>
-                <div class="col-sm-10">
-                  <input v-model="file.name" class="form-control">
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-sm-2 control-label">后缀</label>
-                <div class="col-sm-10">
-                  <input v-model="file.suffix" class="form-control">
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-sm-2 control-label">大小</label>
-                <div class="col-sm-10">
-                  <input v-model="file.size" class="form-control">
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-sm-2 control-label">用途</label>
-                <div class="col-sm-10">
-                  <select v-model="file.use" class="form-control">
-                    <option v-for="o in FILE_USE" v-bind:value="o.key">{{o.value}}</option>
-                  </select>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-            <button v-on:click="save()" type="button"  class="btn btn-primary">保存</button>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
   </div>
 </template>
 
@@ -158,45 +57,6 @@
       // this.$parent().activeSidebar("file-file-sidebar")
     },
     methods: {
-
-      /**
-       * 添加模态框
-       */
-      add() {
-        let _this = this;
-        _this.file = {}; // 解决新增数据时模态框保留上次数据问题
-        // 第一个modal是css选择器，模态框代码里有class="modal",是modal()方法里的是内置方法，弹出关闭模态框hide，show
-        $("#form-modal").modal({backdrop:"static"}, "show"); // 点空白不会关闭
-        // $("#form-modal").modal("show"); // 点空白会关闭
-      },
-
-      /**
-       * 点击【编辑】
-       * */
-      edit(file) {
-        let _this = this;
-        // vue中将表格行数据显示到表单，反过来也会的问题：$.extend({},file)解决(file,复制给空对象)
-        _this.file = $.extend({}, file); //vue中的_tis.file会通过v-modal属性和form表单做数据绑定
-        $("#form-modal").modal({backdrop:"static"}, "show"); // 点空白不会关闭
-
-      },
-
-      /**删除**/
-      del(id) {
-        let _this = this;
-        Confirm.show("删除文件后不可恢复！确认删除？",function () {
-          Loading.show();
-          _this.$ajax.delete(process.env.VUE_APP_SERVER + '/file/admin/file/delete/' + id).then((response)=>{
-            Loading.hide();
-            let resp = response.data;
-            if (resp.success) {
-              _this.list(1);
-              Toast.success("删除成功！");
-            }
-          });
-        });
-      },
-
       /**
        * 列表查询展示分页
        * @param page
@@ -215,35 +75,6 @@
           _this.$refs.pagination.render(page, resp.content.total);//重现渲染组件
         })
       },
-
-      /**点击保存**/
-      save() {
-        let _this = this;
-
-        // 保存校验
-        if (1 != 1
-          || !Validator.require(_this.file.path, "相对路径")
-          || !Validator.length(_this.file.path, "相对路径", 1, 100)
-          || !Validator.length(_this.file.name, "文件名", 1, 100)
-          || !Validator.length(_this.file.suffix, "后缀", 1, 10)
-        ) {
-          return;
-        }
-
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/file/save',
-          _this.file).then((response)=>{
-          Loading.hide();
-          let resp = response.data;
-          if (resp.success) {
-            $("#form-modal").modal("hide");
-            _this.list(1);
-            Toast.success("保存成功！");
-          } else {
-            Toast.warning(resp.message)
-          }
-        })
-      }
     }
   }
 </script>
--- file/src/main/java/com/course/file/controller/admin/FileController.java
+++ file/src/main/java/com/course/file/controller/admin/FileController.java
@@ -12,14 +12,15 @@ package com.course.file.controller.admin;/**
  * Created by 111 on 2021/3/3.
  */
 
-import com.course.server.dto.FileDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.FileService;
-import com.course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -51,39 +52,6 @@ public class FileController {
         ResponseDto responseDto = new ResponseDto();
         fileService.list(pageDto);
         responseDto.setContent(pageDto);
-        return responseDto;
-    }
-
-    /**
-     * 保存，id有值时更新，无值是新增
-     * @param fileDto
-     * @return responseDto
-     */
-//    @RequestMapping("/save")
-    @PostMapping("/save")
-    public ResponseDto save(@RequestBody FileDto fileDto) {
-
-        // 保存的有效性校验
-        ValidatorUtil.require(fileDto.getPath(), "相对路径");
-        ValidatorUtil.length(fileDto.getPath(), "相对路径", 1, 100);
-        ValidatorUtil.length(fileDto.getName(), "文件名", 1, 100);
-        ValidatorUtil.length(fileDto.getSuffix(), "后缀", 1, 10);
-
-        ResponseDto responseDto = new ResponseDto();
-        fileService.save(fileDto);
-        responseDto.setContent(fileDto);
-        return responseDto;
-    }
-
-    /**
-     * 删除
-     * @param id
-     * @return responseDto
-     */
-    @DeleteMapping("/delete/{id}")
-    public ResponseDto delete(@PathVariable String id) {
-        ResponseDto responseDto = new ResponseDto();
-        fileService.delete(id);
         return responseDto;
     }
 }
