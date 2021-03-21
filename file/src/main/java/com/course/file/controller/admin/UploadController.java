317967001be15268c49743a685352db9d3ed7bbb
mangmangxiyu <945353778@qq.com>
2021-03-22 00:46:21 +0800

    1. 大文件断点续传和极速秒传：文件分片上传后触发合并

--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -59,7 +59,7 @@ public class UploadController {
     private FileService fileService;
 
     @RequestMapping("/upload")
-    public ResponseDto upload(@RequestBody FileDto fileDto) throws IOException {//@RequestParam用来接收表单参数的
+    public ResponseDto upload(@RequestBody FileDto fileDto) throws Exception {//@RequestParam用来接收表单参数的
         LOG.info("上传文件开始");
         String use = fileDto.getUse();
         String key = fileDto.getKey();
@@ -80,15 +80,18 @@ public class UploadController {
         // File.separator 文件分隔符：\虽然斜杠"/"也能识别，最好用反斜杠"\"
         // 拼接字符串超过5个就用stringbuffer
 //        String path = dir + File.separator + key +  "." + suffix + "." + fileDto.getShardIndex();/
+        //        String path = dir + File.separator + key + "." + suffix + "." + fileDto.getShardIndex();
         String path = new StringBuffer(dir)
                 .append(File.separator)
                 .append(key)
                 .append(".")
                 .append(suffix)
+                .toString(); // course\6sfSqfOwzmik4A4icMYuUe.mp4
+        String localPath = new StringBuffer(path)
                 .append(".")
                 .append(fileDto.getShardIndex())
-                .toString();
-        String fullPath = FILE_PATH + path;
+                .toString(); // course\6sfSqfOwzmik4A4icMYuUe.mp4.1
+        String fullPath = FILE_PATH + localPath;
         File dest = new File(fullPath);// dest 生成的目标位置
         shard.transferTo(dest);// 文件写到目标位置
         LOG.info(dest.getAbsolutePath());// 打印目标位置全路径
@@ -100,34 +103,31 @@ public class UploadController {
         ResponseDto responseDto = new ResponseDto();
         fileDto.setPath(FILE_DOMAIN + path);
         responseDto.setContent(fileDto);
+
+        if (fileDto.getShardIndex() == fileDto.getShardTotal()) {
+            this.merge(fileDto);
+        }
         return responseDto;
     }
 
     @GetMapping("/merge")
-    public ResponseDto merge() throws Exception {
-        File newFile = new File(FILE_PATH + "course/test123.mp4");
+    public void merge(FileDto fileDto) throws Exception {
+        LOG.info("文件合并---开始");
+        String path = fileDto.getPath();
+        path = path.replace(FILE_DOMAIN, "");
+        File newFile = new File(FILE_PATH + path);
         FileOutputStream outputStream = new FileOutputStream(newFile, true);// 文件追加写入
         FileInputStream fileInputStream = null;// 分片文件
         byte[] byt = new byte[10 * 1024 * 1024];
         int len;
 
         try {
-            // 读取第一个分片
-            fileInputStream = new FileInputStream(new File(FILE_PATH + "course/ALjhUDHf.blob"));
-            while((len = fileInputStream.read(byt)) != -1) {
-                outputStream.write(byt, 0, len);
-            }
-
-            // 读取第二个分片
-            fileInputStream = new FileInputStream(new File(FILE_PATH + "course/a1QCTGzH.blob"));
-            while ((len = fileInputStream.read(byt)) != -1) {
-                outputStream.write(byt, 0, len);
-            }
-
-            // 读取第三个分片
-            fileInputStream = new FileInputStream(new File(FILE_PATH + "course/dDk20pJS.blob"));
-            while ((len = fileInputStream.read(byt)) != -1) {
-                outputStream.write(byt, 0, len);
+            for (int i = 0; i < fileDto.getShardTotal(); i++) {
+                // 读取第一个分片
+                fileInputStream = new FileInputStream(new File(FILE_PATH + path + "." + (i + 1)));
+                while((len = fileInputStream.read(byt)) != -1) {
+                    outputStream.write(byt, 0, len);
+                }
             }
         } catch (IOException e) {
             LOG.error("分片合并异常", e);
@@ -142,7 +142,6 @@ public class UploadController {
                 LOG.error("IO流关闭", e);
             }
         }
-        ResponseDto responseDto = new ResponseDto();
-        return responseDto;
+        LOG.info("文件合并---开始");
     }
 }
