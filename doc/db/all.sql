dbed490709949b04ea732336234acf01db65f285
mangmangxiyu <945353778@qq.com>
2021-03-22 22:21:21 +0800

    1. 视频加密与授权播放: 小节增加VOD字段（外加视频这两个字段隐藏）

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -75,6 +75,8 @@ create table `section` (
   primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='小节';
 
+alter table `section` add column (`vod` char(32) comment 'vod|阿里云vod');
+
 insert into `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, updated_at)
 values ('00000001', '测试小节01', '00000001', '00000000', '', 500, 'F', 1, now(), now());
 
