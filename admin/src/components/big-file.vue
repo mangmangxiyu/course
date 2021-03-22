b26adddc8c08435d59e96947cc41d7ee707d9d3d
mangmangxiyu <945353778@qq.com>
2021-03-22 16:57:31 +0800

    1. 大文件断点续传和极速秒传：上传文件过程中增加进度条组件progress.js

--- admin/src/components/big-file.vue
+++ admin/src/components/big-file.vue
@@ -129,22 +129,28 @@
         let fileShard = _this.getFileShard(shardIndex, shardSize);
         // 将图片转为base64传输
         let fileReader = new FileReader();
+
+        Progress.show(parseInt((shardIndex - 1) * 100 / shardTotal));
         fileReader.onload = function (e) {// 数据onload监听
           let base64 = e.target.result;
           // console.log("base64", base64);
           param.shard = base64;
 
+         // Loading.show();有进度条不需要loading
 
-          Loading.show();
           _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', param).then((response) => {
-            Loading.hide();
+
+            // Loading.hide();
+
             let resp = response.data;
             console.log("上传文件成功：", resp);
+            Progress.show(parseInt(shardIndex * 100 / shardTotal));
             if (shardIndex < shardTotal) {
               // 上传下一个分片
               param.shardIndex = param.shardIndex + 1;
               _this.upload(param);
             } else {
+              Progress.hide();
               _this.afterUpload(resp);
               $("#" + _this.inputId + "-input").val("");
             }
