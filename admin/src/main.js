3c15225c3a2b3626d25cd3c85fa4dab222012f89
mangmangxiyu <945353778@qq.com>
2021-03-25 16:14:04 +0800

    1. 用户管理和登陆：在请求headers中统一增加token，main.js的axios拦截器中配置 2. 系统级别的参数也可以通过拦截器的方式增加到request header中 3. 可以在header中设置请求流水和请求时间

--- admin/src/main.js
+++ admin/src/main.js
@@ -33,6 +33,11 @@ router.beforeEach((to, from, next) => {
  */
 axios.interceptors.request.use(function (config) {
   console.log("请求：", config);
+  let token = Tool.getLoginUser().token;
+  if (Tool.isNotEmpty(token)) {
+    config.headers.token = token;
+    console.log("请求headers增加token：", token);
+  }
   return config;
 }, error => {});
 axios.interceptors.response.use(function (response) {
