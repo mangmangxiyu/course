a3a44572d26b25c63bc4ee69963faa138349aa00
mangmangxiyu <945353778@qq.com>
2021-03-28 00:44:02 +0800

    1. 网站首页开发: 增加课程card组件the-course

---
+++ web/src/components/the-course.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card mb-4 shadow-sm course">
+    <img class="img-fluid" v-bind:src="course.image">
+    <div class="card-body">
+      <h4 class="">{{course.name}}</h4>
+      <p class="card-text">{{course.summary}}</p>
+      <div class="d-flex justify-content-between align-items-center">
+        <div class="btn-group">
+          <button type="button" class="btn btn-sm btn-outline-secondary">课程详情</button>
+        </div>
+        <div class="text-muted">
+          <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{course.price}}</span>&nbsp;
+          <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{course.enroll}}</span>&nbsp;
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style></style>
+<script>
+  export default {
+    name: "the-course",
+    props:{
+      course: {}
+    },
+    data: function () {
+      return {}
+    },
+  }
+</script>
+
+<style>
+  .course h4 {
+    font-size: 1.25rem;
+    margin: 15px;
+  }
+
+  .course .text-muted .badge {
+    font-size: 1rem;
+  }
+</style>
--- web/src/views/index.vue
+++ web/src/views/index.vue
@@ -16,23 +16,7 @@
         <div class="title">最新上线</div>
         <div class="row">
           <div v-for="o in news" class="col-md-4">
-            <div class="card mb-4 shadow-sm course">
-              <img class="img-fluid" v-bind:src="o.image">
-              <div class="card-body">
-                <h4 class="">{{o.name}}</h4>
-                <p class="card-text">{{o.summary}}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <div class="text-muted">
-                    <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{o.price}}</span>&nbsp;
-                    <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{o.enroll}}</span>&nbsp;
-                  </div>
-                </div>
-              </div>
-            </div>
+            <the-course v-bind:course="o"></the-course>
           </div>
         </div>
           <hr>
@@ -40,23 +24,7 @@
         <div class="title2">好课推荐</div>
         <div class="row">
           <div v-for="o in news" class="col-md-4">
-            <div class="card mb-4 shadow-sm course">
-              <img class="img-fluid" v-bind:src="o.image">
-              <div class="card-body">
-                <h4 class="">{{o.name}}</h4>
-                <p class="card-text">{{o.summary}}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <div class="text-muted">
-                    <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{o.price}}</span>&nbsp;
-                    <span class="badge badge-info"><i class="fa fa-yen" aria-hidden="true"></i>&nbsp;{{o.enroll}}</span>&nbsp;
-                  </div>
-                </div>
-              </div>
-            </div>
+            <the-course v-bind:course="o"></the-course>
           </div>
         </div>
       </div>
@@ -67,8 +35,10 @@
 
 <script>
 
+  import TheCourse from "../components/the-course"
   export default {
     name: "index",
+    components: {TheCourse},
     data: function () {
       return {
         news: [],
